706c3327ad941c16feaad0d33e7c673f71cbdcd6
Lee Hinman <lee@writequit.org>
2011-08-09 08:45:43 -0600

    ido, Y U BREAK rgrep EVERYWHERE??

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -495,16 +495,16 @@
       (let (ido-enable-replace-completing-read) ad-do-it))")
 
 ;; Replace completing-read wherever possible, unless directed otherwise
-(defadvice completing-read
-  (around use-ido-when-possible activate)
-  (if (or (not ido-enable-replace-completing-read) ; Manual override disable ido
-          (and (boundp 'ido-cur-list)
-               ido-cur-list)) ; Avoid infinite loop from ido calling this
-      ad-do-it
-    (let ((allcomp (all-completions "" collection predicate)))
-      (if allcomp
-          (setq ad-return-value
-                (ido-completing-read prompt
-                                     allcomp
-                                     nil require-match initial-input hist def))
-        ad-do-it))))
+;; (defadvice completing-read
+;;   (around use-ido-when-possible activate)
+;;   (if (or (not ido-enable-replace-completing-read) ; Manual override disable ido
+;;           (and (boundp 'ido-cur-list)
+;;                ido-cur-list)) ; Avoid infinite loop from ido calling this
+;;       ad-do-it
+;;     (let ((allcomp (all-completions "" collection predicate)))
+;;       (if allcomp
+;;           (setq ad-return-value
+;;                 (ido-completing-read prompt
+;;                                      allcomp
+;;                                      nil require-match initial-input hist def))
+;;         ad-do-it))))

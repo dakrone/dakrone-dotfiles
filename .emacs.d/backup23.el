dfba840eba8557d3f82df573e8c49582136c9838
Lee Hinman <lee@writequit.org>
2011-08-26 12:11:00 -0600

    update emacs23 stuff for minimal usable emacs

--- .emacs.d/backup23.el
+++ .emacs.d/backup23.el
@@ -1,4 +1,34 @@
 (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 (custom-set-faces
- '(comint-highlight-prompt ((t (:foreground "white")))))
+ '(comint-highlight-prompt ((t (:foreground "green")))))
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; ==== IDO ====
+(ido-mode t)
+(setq ido-enable-flex-matching t)   ; enable fuzzy matching
+(setq ido-execute-command-cache nil)
+(defun ido-execute-command ()
+  (interactive)
+  (call-interactively
+   (intern
+    (ido-completing-read
+     "M-x "
+     (progn
+       (unless ido-execute-command-cache
+         (mapatoms (lambda (s)
+                     (when (commandp s)
+                       (setq ido-execute-command-cache
+                             (cons (format "%S" s)
+                                   ido-execute-command-cache))))))
+       ido-execute-command-cache)))))
+
+(add-hook 'ido-setup-hook
+          (lambda ()
+            (setq ido-enable-flex-matching t)
+            (global-set-key "\M-x" 'ido-execute-command)))
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)

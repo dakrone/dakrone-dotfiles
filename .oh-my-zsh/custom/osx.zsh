ced6eec2dba6e57026cd9055a510f2c79dd5a334
Lee Hinman <lee@writequit.org>
2011-09-29 16:09:24 -0600

    some java changes

--- .oh-my-zsh/custom/osx.zsh
+++ .oh-my-zsh/custom/osx.zsh
@@ -2,8 +2,8 @@ OS=$(uname)
 
 if [[ $OS == "Darwin" ]]; then
 
-    #export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
-    export JAVA_HOME=/Library/Java/JavaVirtualMachines/1.7.0.jdk/Contents/Home
+    export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
+    #export JAVA_HOME=/Library/Java/JavaVirtualMachines/1.7.0.jdk/Contents/Home
 
 
     export EMACS_HOME="/Applications/Emacs.app/Contents/MacOS"

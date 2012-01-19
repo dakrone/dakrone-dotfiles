f8e4bcad377e7b062b48da1a9c99a695ef6dcdf9
Lee Hinman <lee@writequit.org>
2012-01-19 10:31:08 -0700

    add url browser to emacs config

--- .emacs.d/hinmanm/bindings.el
+++ .emacs.d/hinmanm/bindings.el
@@ -137,3 +137,17 @@
 (global-set-key (kbd "C-c F") 'iy-go-to-char-backward)
 (global-set-key (kbd "C-c ;") 'iy-go-to-char-continue)
 (global-set-key (kbd "C-c ,") 'iy-go-to-char-continue-backward)
+
+(defun browse-last-url-in-brower ()
+  (interactive)
+  (save-excursion
+    (let ((ffap-url-regexp
+           (concat
+            "\\("
+            "news\\(post\\)?:\\|mailto:\\|file:"
+            "\\|"
+            "\\(ftp\\|https?\\|telnet\\|gopher\\|www\\|wais\\)://"
+            "\\).")))
+      (ffap-next-url t t))))
+
+(global-set-key (kbd "C-c u") 'browse-last-url-in-brower)

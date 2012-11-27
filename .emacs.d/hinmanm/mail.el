d67b99a07e44ef4290dd9b37772879203074edbf
Lee Hinman <lee@writequit.org>
2012-11-27 11:14:57 -0700

    add dired attachment support for mu4e

--- .emacs.d/hinmanm/mail.el
+++ .emacs.d/hinmanm/mail.el
@@ -25,6 +25,23 @@
 (setq pgg-default-user-id "3acecae0")
 (setq epg-gpg-program "/usr/local/bin/gpg")
 
+(require 'gnus-dired)
+;; make the `gnus-dired-mail-buffers' function also work on
+;; message-mode derived modes, such as mu4e-compose-mode
+(defun gnus-dired-mail-buffers ()
+  "Return a list of active message buffers."
+  (let (buffers)
+    (save-current-buffer
+      (dolist (buffer (buffer-list t))
+        (set-buffer buffer)
+        (when (and (derived-mode-p 'message-mode)
+                   (null message-sent-message-via))
+          (push (buffer-name buffer) buffers))))
+    (nreverse buffers)))
+
+(setq gnus-dired-mail-mode 'mu4e-user-agent)
+(add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
+
 ;; David's (and now my) config
 
  ;; Vars used below

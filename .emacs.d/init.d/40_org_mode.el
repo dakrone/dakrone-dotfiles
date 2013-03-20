3f2e3f2a65ca73c70adbe411721bb27081f8f4d3
Lee Hinman <lee@writequit.org>
2013-03-20 12:17:51 -0600

    rework org capture templates

--- .emacs.d/init.d/40_org_mode.el
+++ .emacs.d/init.d/40_org_mode.el
@@ -4,32 +4,22 @@
   (setq org-startup-truncated nil)
   (setq org-return-follows-link t)
   (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-  ;; (org-remember-insinuate)
   (setq org-directory user-emacs-directory)
-;;  (setq org-startup-folded 'nofold)
   (setq org-use-fast-todo-selection t)
   (setq org-todo-keywords
-        '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "DONE(x)" "CANCEL(c)")
-          (sequence "APPT(a)" "|" "DONE(x)" "CANCEL(c)")))
+        '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)"
+                    "|" "DONE(x)" "CANCEL(c)")
+          (sequence "WAITING(f)" "|" "DONE(x)" "CANCEL(c)")))
   (setq org-todo-keyword-faces
-        '(("TODO"      . org-warning)
+        '(;;("TODO"      . org-warning)
           ("STARTED"   . (:foreground "deep sky blue" :weight bold))
           ("DONE"      . (:foreground "SpringGreen1" :weight bold))
-          ("WAITING"   . (:foreground "orange" :weight bold))))
-
-  ;; faces
-  ;; (set-face-foreground 'org-block "green")
-  ;; (set-face-foreground 'org-tag "green yellow")
-
-  ;; function of org-open-at-point
-  ;; (setf (cdr (assoc 'file org-link-frame-setup)) 'find-file))
-  )
+          ("WAITING"   . (:foreground "orange" :weight bold)))))
 
 (eval-after-load "org"
   '(progn
      (define-key org-mode-map (kbd "C-t") 'org-mark-ring-goto)
      (define-key org-mode-map (kbd "C-M-<return>") 'org-insert-todo-heading)
-     (define-key org-mode-map (kbd "C-c C-i") 'my/org-insert-pomodoro-template)
      (define-key org-mode-map (kbd "C-M-<tab>") 'show-all)
      (local-unset-key (kbd "M-S-<return>"))
 
@@ -40,12 +30,7 @@
          org-mode-map "C-c" '(("C-n" . (outline-next-visible-heading 1))
                               ("C-p" . (outline-previous-visible-heading 1))))
      (setq org-clock-persist 'history)
-     (org-clock-persistence-insinuate)))
-
-(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
-
-(eval-after-load "org"
-  '(progn
+     (org-clock-persistence-insinuate)
      (define-prefix-command 'org-todo-state-map)
 
      (define-key org-mode-map "\C-cx" 'org-todo-state-map)
@@ -54,20 +39,14 @@
        #'(lambda nil (interactive) (org-todo "CANCELLED")))
      (define-key org-todo-state-map "d"
        #'(lambda nil (interactive) (org-todo "DONE")))
-     (define-key org-todo-state-map "f"
-       #'(lambda nil (interactive) (org-todo "DEFERRED")))
-     (define-key org-todo-state-map "l"
-       #'(lambda nil (interactive) (org-todo "DELEGATED")))
+     (define-key org-todo-state-map "t"
+       #'(lambda nil (interactive) (org-todo "TODO")))
      (define-key org-todo-state-map "s"
        #'(lambda nil (interactive) (org-todo "STARTED")))
      (define-key org-todo-state-map "w"
-       #'(lambda nil (interactive) (org-todo "WAITING")))
+       #'(lambda nil (interactive) (org-todo "WAITING")))))
 
-     ;;     (define-key org-agenda-mode-map "\C-n" 'next-line)
-     ;;     (define-key org-agenda-keymap "\C-n" 'next-line)
-     ;;     (define-key org-agenda-mode-map "\C-p" 'previous-line)
-     ;;     (define-key org-agenda-keymap "\C-p" 'previous-line)
-     ))
+(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
 
 (add-hook 'org-mode-hook '(lambda ()
                             (define-key org-mode-map [C-tab] 'other-window)
@@ -86,16 +65,21 @@
 (setq org-agenda-files (list org-directory))
 (setq org-completion-use-ido 't)
 (setq org-refile-targets `((,(concat "~" (user-login-name)
-                                     "/org/todo.org") . (:level . 1))))
+                                     "/org/todo.org") . (:level . 1))
+                           (,(concat "~" (user-login-name)
+                                     "/org/notes.org") . (:level . 1))))
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "todo.org" "Unsorted") "* TODO %i%?")
-        ("n" "Notes" entry (file+headline "notes.org" "Notes") "** %? ")
-        ("j" "Journal" entry (file+datetree "journal.org") "* %i%?")))
+      '(("f" "Follow up" entry (file+headline "todo.org" "Follow up")
+         "* WAITING %i%? %^G\nAdded: %U")
+        ("t" "Todo" entry (file+headline "todo.org" "Unsorted")
+         "* TODO %i%? %^G\nAdded: %U")
+        ("n" "Notes" entry (file+headline "notes.org" "Notes")
+         "* %?\nAdded: %U")
+        ("j" "Journal" entry (file+datetree "journal.org")
+         "* %?\nEntered on %U\n  %i")))
 
 ;;(add-hook 'remember-mode-hook 'org-remember-apply-template)
-
 ;;(define-key global-map [(control meta ?r)] 'remember)
-
 ;; (custom-set-variables
 ;;  '(org-agenda-files (quote ("~/agenda.org" "~/work.org")))
 ;;  '(org-default-notes-file "~/notes.org")

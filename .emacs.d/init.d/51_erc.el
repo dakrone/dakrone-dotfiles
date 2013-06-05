2b702782b9c2ee47fbd1ef5db7b8d63d88325d7e
Lee Hinman <lee@writequit.org>
2013-06-04 22:35:10 -0600

    different fill column, back to smaller

--- .emacs.d/init.d/51_erc.el
+++ .emacs.d/init.d/51_erc.el
@@ -70,7 +70,7 @@
 
 (eval-after-load 'erc
   '(progn
-     (setq erc-fill-column 100
+     (setq erc-fill-column 80
            erc-server-coding-system '(utf-8 . utf-8)
            erc-hide-list '("JOIN" "PART" "QUIT" "NICK")
            erc-track-exclude-types (append '("324" "329" "332" "333"

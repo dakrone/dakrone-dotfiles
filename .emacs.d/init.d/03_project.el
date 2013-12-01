7b7cc0f87f3bede094777845c53e7a75a1ffe57c
Lee Hinman <lee@writequit.org>
2013-12-01 16:04:52 -0700

    add project and eshell stuff

---
+++ .emacs.d/init.d/03_project.el
@@ -0,0 +1,3 @@
+;; Project stuff
+
+(projectile-global-mode)
---
+++ .emacs.d/init.d/05_eshell.el
@@ -0,0 +1,22 @@
+;;; Eshell stuff
+
+(add-hook
+ 'eshell-mode-hook
+ (lambda ()
+   (defalias 'emacs 'find-file)))
+
+(defun eshell/emacs (file)
+  (find-file file))
+
+(defun eshell/less (&rest args)
+  "Invoke `view-file' on a file. \"less +42 foo\" will go to line 42 in
+    the buffer for foo."
+  (while args
+    (if (string-match "\\`\\+\\([0-9]+\\)\\'" (car args))
+        (let* ((line (string-to-number (match-string 1 (pop args))))
+               (file (pop args)))
+          (tyler-eshell-view-file file)
+          (goto-line line))
+      (tyler-eshell-view-file (pop args)))))
+
+(defalias 'eshell/more 'eshell/less)

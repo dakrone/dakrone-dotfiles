04646d267aa06b9726f31beb1182e17b2b658aa1
Lee Hinman <lee@writequit.org>
2014-01-01 10:25:57 -0700

    Add prodigy to start up ES

--- .emacs.d/setup.el
+++ .emacs.d/setup.el
@@ -28,7 +28,7 @@
     expand-region smex windresize ag undo-tree iedit ido-ubiquitous
     ido-vertical-mode yasnippet idle-highlight-mode smart-tab anzu
     smartparens flx-ido projectile smooth-scrolling yascroll keyfreq
-    lusty-explorer discover
+    lusty-explorer discover prodigy
 
     ;; org-mode
     org htmlize gnuplot-mode gnuplot

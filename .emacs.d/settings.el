4888847cbd2a720b43c61e2101220b1992477fbd
Lee Hinman <lee@writequit.org>
2014-02-01 22:26:03 -0700

    org-babel es-mode setup

--- .emacs.d/settings.el
+++ .emacs.d/settings.el
@@ -763,6 +763,12 @@
        (python . t)
        (gnuplot . t)))
 
+    ;; Allow executing es-mode files (https://github.com/dakrone/es-mode)
+    (defun org-babel-execute:es (body params)
+      "Execute a block of ES code with org-babel."
+      (message "executing ES source code block")
+      (org-babel-eval "/bin/sh" body))
+
     ;; Use cider as the clojure backend
     (setq org-babel-clojure-backend 'cider)
 

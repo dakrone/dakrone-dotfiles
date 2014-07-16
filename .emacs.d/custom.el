c2ec7a1fea8b6d951ed440cffd7e6ea39074a971
Lee Hinman <lee@writequit.org>
2014-07-16 19:46:44 +0200

    change org export backends

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -13,7 +13,7 @@
    (quote
     ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "025354235e98db5e7fd9c1a74622ff53ad31b7bde537d290ff68d85665213d85" "6fe6ab4abe97a4f13533e47ae59fbba7f2919583f9162b440dd06707b01f7794" "76b226dd750d085eaaf7efa5eb07a3282223d74f327a0f4319512c0a59f6df39" "9a217ee1dcefd5e83f78381c61e25e9c4d25c7b80bf032f44d7d62ca68c6a384" "fc6e906a0e6ead5747ab2e7c5838166f7350b958d82e410257aeeb2820e8a07a" default)))
  '(delete-selection-mode t)
- '(org-export-backends (quote (ascii html icalendar latex md gfm)))
+ '(org-export-backends (quote (ascii html latex md gfm odt)))
  '(org-modules
    (quote
     (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))

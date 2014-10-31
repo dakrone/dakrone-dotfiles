7d66311ad7e36061930a0531212731797b468bde
Lee Hinman <lee@writequit.org>
2014-10-31 12:34:43 +0100

    New custom.el customizations

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -7,6 +7,12 @@
    [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(bmkp-last-as-first-bookmark-file "/Users/hinmanm/.emacs.d/bookmarks")
+ '(c-default-style
+   (quote
+    ((java-mode . "intellij")
+     (awk-mode . "awk")
+     (other . "gnu"))))
  '(comint-buffer-maximum-size 20000)
  '(comint-completion-addsuffix t)
  '(comint-get-old-input (lambda nil "") t)
@@ -16,13 +22,18 @@
  '(comint-prompt-read-only nil)
  '(comint-scroll-show-maximum-output t)
  '(comint-scroll-to-bottom-on-input t)
+ '(compilation-message-face (quote default))
  '(compilation-scroll-output t)
  '(compilation-skip-threshold 2)
  '(custom-safe-themes
    (quote
-    ("3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "53e29ea3d0251198924328fd943d6ead860e9f47af8d22f0b764d11168455a8e" "96b023d1a6e796bab61b472f4379656bcac67b3af4e565d9fb1b6b7989356610" "696927ed40057050d219989dd473cc08fab27995e4febd6cf14429a448364bc6" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "e8ff60c7811d4532ee9f756b654d2f13d455e04851ee60c5e033e1b6a17e968f" "e515bdcabbbd48e8dbcb086353f29c8c6ca1f04f8b61c287056db66c87559ed5" "75c9f0b0499ecdd0c856939a5de052742d85af81814e84faa666522c2bba7e85" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "025354235e98db5e7fd9c1a74622ff53ad31b7bde537d290ff68d85665213d85" "6fe6ab4abe97a4f13533e47ae59fbba7f2919583f9162b440dd06707b01f7794" "76b226dd750d085eaaf7efa5eb07a3282223d74f327a0f4319512c0a59f6df39" "9a217ee1dcefd5e83f78381c61e25e9c4d25c7b80bf032f44d7d62ca68c6a384" "fc6e906a0e6ead5747ab2e7c5838166f7350b958d82e410257aeeb2820e8a07a" default)))
+    ("60afae06e6d70543aa744f6e775f97ad8f1cc68ce46b3caa7ef746980d15120b" "6635d314200b51445088192fedfd71359bedec0b849821289f2da4897b5b6dd9" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "a6f2d55c6bf894728038e69f51be2c9d3e5d4113fb7dc53cdedb87aceb06bd51" "c7cd81771525ff66c105413134cdf0330b0b5b88fd8096e5d56b0256872ba6c7" "d401b0920023533c5de1553d27e9d4669ae088f771cf2736108870f783af0cc0" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b5790d1235a935a2767bbda6f122e5bbdca4934a31b89ed43ce492577529da6" "1c6c7d5e4beaec0a54d814454106d180de7b90f8961d3edd2f6567f7c08da97e" "46223bc978f9e7ab7f5b61d171da7ce98e69661158b75ed011603d3134fbad02" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "53e29ea3d0251198924328fd943d6ead860e9f47af8d22f0b764d11168455a8e" "96b023d1a6e796bab61b472f4379656bcac67b3af4e565d9fb1b6b7989356610" "696927ed40057050d219989dd473cc08fab27995e4febd6cf14429a448364bc6" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "e8ff60c7811d4532ee9f756b654d2f13d455e04851ee60c5e033e1b6a17e968f" "e515bdcabbbd48e8dbcb086353f29c8c6ca1f04f8b61c287056db66c87559ed5" "75c9f0b0499ecdd0c856939a5de052742d85af81814e84faa666522c2bba7e85" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "025354235e98db5e7fd9c1a74622ff53ad31b7bde537d290ff68d85665213d85" "6fe6ab4abe97a4f13533e47ae59fbba7f2919583f9162b440dd06707b01f7794" "76b226dd750d085eaaf7efa5eb07a3282223d74f327a0f4319512c0a59f6df39" "9a217ee1dcefd5e83f78381c61e25e9c4d25c7b80bf032f44d7d62ca68c6a384" "fc6e906a0e6ead5747ab2e7c5838166f7350b958d82e410257aeeb2820e8a07a" default)))
  '(delete-selection-mode t)
+ '(diredp-hide-details-initially-flag nil)
+ '(esv-key "IP")
+ '(logstash-indent 2)
  '(magit-set-upstream-on-push (quote dontask))
+ '(magit-use-overlays nil)
  '(org-export-backends (quote (ascii html latex md gfm odt)))
  '(org-modules
    (quote
